1c43fae2754feb20e4b22258a10b3095db72f16b
Ugo DANIEL-THOMAS <ugo@air-de-ugo.lan>
2021-09-24 17:06:16 +0200

    small improvements, added basic write functions (no fd)

--- srcs/write/_ft_write.h
+++ srcs/write/_ft_write.h
@@ -6,7 +6,7 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 15:40:43 by ugdaniel          #+#    #+#             */
-/*   Updated: 2021/09/23 21:35:35 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2021/09/24 15:39:06 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,28 @@
 
 # include <unistd.h>
 
-/* Print a character to the file descriptor fd. */
+/* Print the character c on the standard output. */
+void	ft_putchar(const char c);
+
+/* Print the  character c on the file descriptor fd. */
 void	ft_putchar_fd(const char c, int fd);
 
-/* Print a string to the file descriptor fd. */
+/* Print the string s on the standard output. */
+void	ft_putstr(const char *s);
+
+/* Print the string s on the file descriptor fd. */
 void	ft_putstr_fd(const char *s, int fd);
 
-/* Print a string followed by a newline, to the file descriptor fd. */
+/* Print the string s followed by a newline, on the standard output. */
+void	ft_putendl(const char *s);
+
+/* Print a string followed by a newline, on the file descriptor fd. */
 void	ft_putendl_fd(const char *s, int fd);
 
-/* Print a number to the file descriptor fd. */
+/* Print a number on the standard output. */
+void	ft_putnbr(int n);
+
+/* Print a number on the file descriptor fd. */
 void	ft_putnbr_fd(int n, int fd);
 
 #endif /* _FT_WRITE_H */
--- srcs/write/ft_putendl.c
+++ srcs/write/ft_putendl.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putendl.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/18 14:39:27 by ugdaniel          #+#    #+#             */
-/*   Updated: 2021/09/24 15:41:07 by ugdaniel         ###   ########.fr       */
+/*   Created: 2020/11/18 14:38:08 by ugdaniel          #+#    #+#             */
+/*   Updated: 2021/09/24 15:42:33 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "_ft_write.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_putendl(const char *s)
 {
-	if (n == -2147483648)
-	{
-		ft_putnbr_fd(-214748364, fd);
-		ft_putchar_fd('8', fd);
-	}
-	else if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(-n, fd);
-	}
-	else if (n < 10)
-		ft_putchar_fd(n + 48, fd);
-	else
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
-	}
+	ft_putendl_fd(s, STDOUT_FILENO);
 }
