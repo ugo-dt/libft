3b687981e76497d7f4dac32ed45e9795a15d5acd
Ugo DANIEL-THOMAS <ugo44dt@gmail.com>
2021-10-02 19:38:28 +0200

    minor changes

--- srcs/write/_ft_write.h
+++ srcs/write/_ft_write.h
@@ -6,7 +6,7 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 15:40:43 by ugdaniel          #+#    #+#             */
-/*   Updated: 2021/10/01 19:24:01 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2021/10/02 19:26:07 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,22 @@ extern void	ft_putchar(char c);
 /* Write a character on the file descriptor fd. */
 extern void	ft_putchar_fd(char c, int fd);
 
-/* Prints the string s on the standard output. */
+/* Print the string s on the standard output. */
 extern void	ft_putstr(const char *s);
 
-/* Prints the string s on the file descriptor fd. */
+/* Print the string s on the file descriptor fd. */
 extern void	ft_putstr_fd(const char *s, int fd);
 
-/* Prints the string s followed by a newline, on the standard output. */
+/* Print the string s followed by a newline, on the standard output. */
 extern void	ft_putendl(const char *s);
 
-/* Prints a string followed by a newline, on the file descriptor fd. */
+/* Print a string followed by a newline, on the file descriptor fd. */
 extern void	ft_putendl_fd(const char *s, int fd);
 
-/* Prints a number on the standard output. */
+/* Print a number on the standard output. */
 extern void	ft_putnbr(int n);
 
-/* Prints a number on the file descriptor fd. */
+/* Print a number on the file descriptor fd. */
 extern void	ft_putnbr_fd(int n, int fd);
 
 /* _FT_WRITE_H */
