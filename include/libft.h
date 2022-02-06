51bb1c8e917a938e9297d43ca369259d577beafb
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-02-06 13:07:07 +0100

    fix

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 11:48:35 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/02/06 13:02:08 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/02/06 13:06:49 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 # define LIBFT_H	1
 
 # define _LIBFT_HEADER	1
+
+# include "_ft_array.h"
+# include "_ft_char.h"
+# include "_ft_list.h"
+# include "_ft_mem.h"
+# include "_ft_printf.h"
+# include "_ft_string.h"
+# include "_ft_write.h"
 
 /* LIBFT_H */
 #endif
