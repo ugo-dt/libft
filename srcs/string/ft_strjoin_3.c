6ff9c6a76aa3fc8db4cde9f7d689469807a51db3
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-03-26 17:43:35 +0100

    norme

--- srcs/string/ft_strjoin_3.c
+++ srcs/string/ft_strjoin_3.c
@@ -6,7 +6,7 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 12:18:38 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/03/26 13:02:24 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/03/26 17:41:56 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ char	*ft_strjoin_3(const char *s1, const char *s2, const char *s3)
 
 	if (!s1 || !s2 || !s3)
 		return (NULL);
-	dst = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + ft_strlen(s3)) + 1);
+	dst = malloc(sizeof(char)
+			* (ft_strlen(s1) + ft_strlen(s2) + ft_strlen(s3)) + 1);
 	if (!dst)
 		return (NULL);
 	return (ft_strcat(ft_strcat(ft_strcpy(dst, s1), s2), s3));
