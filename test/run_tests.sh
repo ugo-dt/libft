f7a1d49040a5db89f2f33370e87ff822becd95ab
ugdaniel <ugdaniel.42.fr>
2023-06-11 21:36:39 +0200

    update

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
+current_dir=$(pwd)
+underline="\033[4m"
+reset="\033[0m"
+
 run_test()
 {
-	clang -Wall -Wextra -Werror test/srcs/$1/*.c -o test/srcs/$1/$1.test -I include -I test libft.a
+	clang -Wall -Wextra -Werror test/$1/main.c -o test/$1/$1.test -I include -I test $0/../libft.a
 	printf "$1\n"
-	./test/srcs/$1/$1.test
+	./test/$1/$1.test
 }
 
-run_test array
-# run_test char
-# run_test ft_printf
-# run_test list
-# run_test math
-# run_test mem
-# run_test string
-# run_test write
+run_all_tests()
+{
+	run_test array
+	run_test char
+	run_test ft_printf
+	run_test list
+	run_test math
+	run_test mem
+	run_test string
+	run_test write
+}
+
+if [ "$1" == "all" ]; then
+	run_all_tests
+elif [ -n "$1" ]; then
+	run_test "$1"
+else
+	printf "No test provided.\nSynopsis: $0 ${underline}test${reset}\n\nAvailable tests: \narray\nchar\nft_printf\nlist\nmath\nmem\nstring\nwrite\n"
+fi
+
